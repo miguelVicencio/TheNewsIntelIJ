a454eb993dc1300c81bf6be8dc7d129508f914d2
Miguel Vicencio <miguel.vicenciog@gmail.com>
2021-12-14 03:54:53 -0300

    chore(Project): initialize

---
+++ src/test/java/cl/ucn/disc/dsm/mvicencio/newsapi/TheNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.dsm.mvicencio.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
